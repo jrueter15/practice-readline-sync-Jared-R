b862dba5353636d087d835b3c50106f15a8aa2ca
jrueter15 <jrueter15@gmail.com>
2025-03-19 21:27:23 -0500

    Print Their Answers Back

--- app.js
+++ app.js
@@ -5,6 +5,7 @@ let name = readline.question("What is your name? "); // Ask the user their name
 console.log("Hello " + name + " welcome!"); // Output the user's response
 
 let data1 = readline.question("What is this kind of data: 'Tourney Time' : ");
+console.log(data1);
 if (data1 === "string" ){
   console.log("Correct!");
 } else {
@@ -12,6 +13,7 @@ if (data1 === "string" ){
 }
 
 let data2 = readline.question("Enter an example of a number data type: ");
+console.log(data2);
 data2 = Number(data2);
 if (isNaN(data2)) {
   console.log("That is not a number");
@@ -20,6 +22,7 @@ if (isNaN(data2)) {
 }
 
 let data3 = readline.question("What is this kind of data 'true' : ");
+console.log(data3);
 if (data3 === "boolean") {
   console.log("Correct!");
 } else {
@@ -27,6 +30,7 @@ if (data3 === "boolean") {
 }
 
 let data4 = readline.question("What is the code output of: '5' + 2? ");
+console.log(data4);
 if (data4 === "52") {
   console.log("Correct!");
 } else {
@@ -34,6 +38,7 @@ if (data4 === "52") {
 }
 
 let data5 = readline.question("What type of data is NaN? ");
+console.log(data5);
 if (data5 === "number") {
   console.log("Correct!");
 } else {
